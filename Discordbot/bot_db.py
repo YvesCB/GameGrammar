e4fd35ec40c9e4eeeef85096df0d5fc916a8b69b
YvesCB <yves@Yves-Desktop.localdomain>
2020-06-13 20:01:08 +0200

    Role assignment and other stuff

--- Discordbot/bot_db.py
+++ Discordbot/bot_db.py
@@ -3,8 +3,9 @@ from tinydb import TinyDB, Query
 db = TinyDB('./data/db.json')
 db_tags = db.table('tags')
 db_admin_roles = db.table('admin_roles')
+db_user_roles = db.table('user_roles')
 
-
+# functions for tag db
 def get_tag(name):
     Tag = Query()
     tags = db_tags.search(Tag.name == name)
@@ -31,7 +32,7 @@ def remove_tag(name):
     Tag = Query()
     return db_tags.remove(Tag.name == name)
 
-
+# functions for admin role db
 def get_admin_role(name):
     Admin_role = Query()
     admins = db_admin_roles.search(Admin_role.name == name)
@@ -39,6 +40,15 @@ def get_admin_role(name):
         return admins[0]
     else:
         return None
+
+
+def exists_admin_role(name):
+    admin_role = get_admin_role(name)
+    return admin_role is not None
+    
+
+def get_all_admin_roles():
+    return db_admin_roles.all()
 
 
 def add_admin_role(name):
@@ -49,11 +59,29 @@ def remove_admin_role(name):
     Admin_role = Query()
     return db_admin_roles.remove(Admin_role.name == name)
 
+# functions for user assignable roles db
+def get_user_role(name):
+    User_role = Query()
+    roles = db_user_roles.search(User_role.name == name)
+    if len(roles) > 0:
+        return roles[0]
+    else:
+        return None
 
-def get_all_admin_roles():
-    return db_admin_roles.all()
+
+def exists_user_role(name):
+    user_role = get_user_role(name)
+    return user_role is not None
 
 
-def exists_admin_role(name):
-    admin_role = get_admin_role(name)
-    return admin_role is not None
+def get_all_user_roles():
+    return db_user_roles.all()
+
+
+def add_user_role(name):
+    db_user_roles.insert({'name': name})
+
+
+def remove_user_role(name):
+    User_role = Query()
+    return db_user_roles.remove(User_role.name == name)
--- Discordbot/grammarbot.py
+++ Discordbot/grammarbot.py
@@ -2,11 +2,24 @@ import discord
 from discord.ext import commands
 import random
 
+
 import config
 import bot_db
 import bot_tools
 
+
 bot = commands.Bot(command_prefix=config.command_prefix)
+
+
+def is_admin():
+    async def predicate(ctx):
+        if bot_tools.is_admin(ctx.author) or ctx.author.id == config.default_admin_id:
+            return True
+        else:
+            await ctx.send('**You must be an admin to use this command!**')
+            return False
+    return commands.check(predicate)
+
 
 @bot.event
 async def on_ready():
@@ -16,10 +29,12 @@ async def on_ready():
         f'{guild.name}(id: {guild.id})'
     )
 
+
 @bot.command(name='tags', help='This command will list all available tags.')
 async def list_tags(ctx):
     tags = [t['name'] for t in bot_db.get_all_tags()]
     await ctx.send('The available tags are: ```\n{}\n```'.format('\n'.join(tags)))
+
 
 @bot.command(name='t', help='Call pre-written messages called tags.')
 async def get_tag(ctx):
@@ -35,85 +50,153 @@ async def get_tag(ctx):
     else:
         await ctx.send(tag['response'])
 
+
 @bot.command(name='t_add', help='Add a new tag to the list of tags. Usage: !t_add <name> <message>')
+@is_admin()
 async def add_tag(ctx):
-    if bot_tools.is_admin(ctx.author):
-        try:
-            command = bot_tools.parse_command(ctx.message.content, 2)
-        except ValueError:
-            await ctx.send('Error: To create tags, use !t_add followed by the name followed by the messsage.')
-            return
-        [_, tag_name, tag_content] = command
-        if bot_db.exists_tag(tag_name):
-            await ctx.send(f'Error: The tag `{tag_name}` already exists.')
-            return
-        else:
-            bot_db.add_tag(tag_name, tag_content)
-            await ctx.send(f'Successfully added tag `{tag_name}`!')
+    try:
+        command = bot_tools.parse_command(ctx.message.content, 2)
+    except ValueError:
+        await ctx.send('Error: To create tags, use !t_add followed by the name followed by the messsage.')
+        return
+    [_, tag_name, tag_content] = command
+    if bot_db.exists_tag(tag_name):
+        await ctx.send(f'Error: The tag `{tag_name}` already exists.')
+        return
     else:
-        await ctx.send('Error: **You must be an admin to use this command!**')
+        bot_db.add_tag(tag_name, tag_content)
+        await ctx.send(f'Successfully added tag `{tag_name}`!')
+
 
 @bot.command(name='t_remove', help='Remove an existing tag. Usage: !t_remove <name>')
+@is_admin()
 async def remove_tag(ctx):
-    if bot_tools.is_admin(ctx.author):
-        try:
-            command = bot_tools.parse_command(ctx.message.content, 1)
-        except ValueError:
-            await ctx.send('Error: To remove a tag, use !t_remove followed by just the name of tag.')
-            return
-        [_, tag_name] = command
-        if not bot_db.exists_tag(tag_name):
-            await ctx.send(f'Error: `{tag_name}` does not exists!')
-            return
-        else:
-            bot_db.remove_tag(tag_name)
-            await ctx.send(f'Sucessfully removed tag `{tag_name}`.')
+    try:
+        command = bot_tools.parse_command(ctx.message.content, 1)
+    except ValueError:
+        await ctx.send('Error: To remove a tag, use !t_remove followed by just the name of tag.')
+        return
+    [_, tag_name] = command
+    if not bot_db.exists_tag(tag_name):
+        await ctx.send(f'Error: `{tag_name}` does not exists!')
+        return
     else:
-        await ctx.send('Erorr: **You must be an admin to use this command!**')
+        bot_db.remove_tag(tag_name)
+        await ctx.send(f'Sucessfully removed tag `{tag_name}`.')
+
 
 @bot.command(name='admins', help='Shows a list of all current admin roles.')
+@command.guild_only()
 async def list_admins(ctx):
     admins = [admins['name'] for admins in bot_db.get_all_admin_roles()]
     await ctx.send('The admin roles are: ```\n{}\n```'.format('\n'.join(admins)))
 
+
 @bot.command(name='admin_add', help='Adds a role to the list of admins. **Can only be used by admins!**')
+@is_admin()
+@command.guild_only()
 async def add_admin(ctx):
-    if ctx.author.id == config.default_admin_id or bot_tools.is_admin(ctx.author):
-        try:
-            command = bot_tools.parse_command(ctx.message.content, 1)
-        except ValueError:
-            await ctx.send('Error: To add a role to admins, use !admin_add followed by the role name.')
-            return
-        [_, role_name] = command
-        if bot_db.exists_admin_role(role_name):
-            await ctx.send(f'Error: The role `{role_name}` is alreayd an admin.`')
-            return
-        else:
-            if role_name in [roles.name for roles in ctx.guild.roles]:
-                bot_db.add_admin_role(role_name)
-                await ctx.send(f'Successfully added the role `{role_name}` to the list of admins.')
-            else:
-                await ctx.send(f'Error: The role `{role_name}` does not exists on this server!')
+    try:
+        command = bot_tools.parse_command(ctx.message.content, 1)
+    except ValueError:
+        await ctx.send('Error: To add a role to admins, use !admin_add followed by the role name.')
+        return
+    [_, role_name] = command
+    if bot_db.exists_admin_role(role_name):
+        await ctx.send(f'Error: The role `{role_name}` is alreayd an admin.`')
+        return
     else:
-        await ctx.send('**You must be an admin to use this command!**')
+        if role_name in [roles.name for roles in ctx.guild.roles]:
+            bot_db.add_admin_role(role_name)
+            await ctx.send(f'Successfully added the role `{role_name}` to the list of admins.')
+        else:
+            await ctx.send(f'Error: The role `{role_name}` does not exists on this server!')
+
 
 @bot.command(name='admin_remove', help='Removes a role from the list of admins. **Can only be used by admins!**')
+@is_admin()
+@command.guild_only()
 async def remove_admin(ctx):
-    if ctx.author.id == config.default_admin_id or bot_tools.is_admin(ctx.author):
+    try:
+        command = bot_tools.parse_command(ctx.message.content, 1)
+    except ValueError:
+        await ctx.send('Error: To remove a role from the list of admins, use !admin_remove followed by the role name.')
+        return
+    [_, role_name] = command
+    if not bot_db.exists_admin_role(role_name):
+        await ctx.send(f'Error: The role `{role_name}` is not on the list of admins!')
+        return
+    else:
+        bot_db.remove_admin_role(role_name)
+        await ctx.send(f'Sucessfully removed the role `{role_name}` fromt the list of amdins!')
+
+
+@bot.command(name='user_role_add', help='Adds a role to the list of self-assignable roles. **Can only be used by admins!**')
+@is_admin()
+@command.guild_only()
+async def add_user_role(ctx):
+    try:
+        command = bot_tools.parse_command(ctx.message.content, 1)
+    except ValueError:
+        await ctx.send('Error: To add a role to the self-assignable roels, use !user_role_add followed by the role name.')
+        return
+    [_, role_name] = command
+    if bot_db.exists_user_role(role_name):
+        await ctx.send(f'Error: The role `{role_name}` is already self-assignable.')
+        return
+    else:
+        if role_name in [roles.name for roles in ctx.guild.roles]:
+            bot_db.add_user_role(role_name)
+            await ctx.send(f'Successfully added the role `{role_name}` to the list of self-assignable roles.')
+        else:
+            await ctx.send(f'Error: the role `{role_name}` does not exits on this server!')
+
+
+@bot.command(name='user_role_remove', help='Removes a role from the list of self-assignable roles.')
+@is_admin()
+@command.guild_only()
+async def remove_user_role(ctx):
+    try:
+        command = bot_tools.parse_command(ctx.message.content, 1)
+    except ValueError:
+        await ctx.send('Error: To remove a role from the list of self-assignable roles, use !user_role_remove followed by the role name.')
+        return
+    [_, role_name] = command
+    if not bot_db.exists_user_role(role_name):
+        await ctx.send(f'Error: The role `{role_name}` is not on the list of self-assignalbe roles!')
+        return
+    else:
+        bot_db.remove_user_role(role_name)
+        await ctx.send(f'Sucessfully removed the role `{role_name}` fromt the list of self-assignable roles!')
+
+
+@bot.command(name='user_role', help='Assigns a role to a user or lists available roles.')
+@command.guild_only()
+async def get_remove_role(ctx):
+    try:
+        command = bot_tools.parse_command(ctx.message.content, 1)
+    except ValueError:
         try:
-            command = bot_tools.parse_command(ctx.message.content, 1)
+            command = bot_tools.parse_command(ctx.message.content, 0)
         except ValueError:
-            await ctx.send('Error: To add a role to admins, use !admin_add followed by the role name.')
+            await ctx.send('Error: To remove/add a role, use !user_role followed by the role name. Just !user_role will list the available roles.')
             return
+    if len(command) == 1:
+        user_roles = [user_roles['name'] for user_roles in bot_db.get_all_user_roles()]
+        await ctx.send('The self-assignable roles are: ```\n{}\n```'.format('\n'.join(user_roles)))
+    else:
         [_, role_name] = command
-        if not bot_db.exists_admin_role(role_name):
-            await ctx.send(f'Error: The role `{role_name}` is not on the list of admins!')
+        if not bot_db.exists_user_role(role_name):
+            await ctx.send(f'Error: The role `{role_name}` is not on the list of self-assignalbe roles!')
             return
         else:
-            bot_db.remove_admin_role(role_name)
-            await ctx.send(f'Sucessfully removed the role `{role_name}` fromt the list of amdins!')
-    else:
-        await ctx.send('**You must be an admin to use this command!**')
-
+            if role_name in [author_roles.name for author_roles in ctx.author.roles]:
+                role = next(role for role in ctx.author.roles if role.name == role_name)
+                await ctx.author.remove_roles(role, reason=None, atomic=True)
+                await ctx.send(f'Removed role `{role_name}` from your roles {ctx.author.name}!')
+            else:
+                role = next(role for role in ctx.guild.roles if role.name == role_name)
+                await ctx.author.add_roles(role, reason=None, atomic=True)
+                await ctx.send(f'Added role `{role_name}` to your roles {ctx.author.name}!')
 
 bot.run(config.discord_token)
