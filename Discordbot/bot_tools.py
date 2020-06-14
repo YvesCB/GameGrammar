ff7b30912aaa8cecbbfc20d81f740ad0e66d3ea7
YvesCB <yves@Yves-Desktop.localdomain>
2020-06-14 15:15:39 +0200

    New Help command, implemented embeds

--- Discordbot/bot_tools.py
+++ Discordbot/bot_tools.py
@@ -1,5 +1,8 @@
+import discord
+
 import config
 import bot_db
+
 
 def parse_command(message, n_arguments=None):
     """
@@ -25,6 +28,7 @@ def parse_command(message, n_arguments=None):
     else:
         return splits[:n_arguments] + [' '.join(splits[n_arguments:])]
 
+
 def common_member(a, b):
     a_set = set(a)
     b_set = set(b)
@@ -32,7 +36,44 @@ def common_member(a, b):
         return(True)
     return(False)
 
+
 def is_admin(user):
     if(common_member([r['name'] for r in bot_db.get_all_admin_roles()], [roles.name for roles in user.roles])):
         return(True)
     return(False)
+
+
+def create_help_embed(commands):
+    help_embed = discord.Embed(
+        title = 'Commands',
+        description = 'Here are all the commands supported by GrammarBot',
+        color = discord.Color.blue()
+    )
+    help_embed.set_footer(text='WIP')
+    for command in commands:
+       help_embed.add_field(name = f'!{command.name}', value=f'{command.help}', inline=False)
+    return help_embed
+    
+
+def create_list_embed(_title, _description, _field_name, items):
+    list_embed = discord.Embed(
+        title = _title,
+        description = _description,
+        color = discord.Color.blue()
+    )
+    list_embed.set_footer(text='WIP')
+    if len(items) == 0:
+        list_embed.add_field(name = 'Roles', value = 'There are currently no user assignable roles. An admin can add them useing `!user_role_add/!ur_add <role_name>`')
+    else:
+        list_embed.add_field(name = _field_name, value = '{}'.format('\n'.join(items)), inline=False)
+    return list_embed
+
+
+def create_simple_embed(_title, _description):
+    sipmle_embed = discord.Embed(
+        title = _title,
+        description = _description,
+        color = discord.Color.blue()
+    )        
+    sipmle_embed.set_footer(text='WIP')
+    return sipmle_embed
