b54c44ea01f1a7a5b4a154565bf3a31045bf1488
Yves Bruckmayer <bryves@student.ethz.ch>
2020-11-20 15:25:38 +0100

    Updated the example config file

--- Discordbot/config_example.py
+++ Discordbot/config_example.py
@@ -1,13 +1,17 @@
-# Grammarbot:
-discord_token = '<token here>'
+discord_token = 'Bot Token here' 
 
-# discord_guild = 'GameGrammar'
-discord_guild = '<your guild name here>'
+discord_guild = 'Guild id here'
 
 command_prefix = '!'
 default_admin_id = 0
+stream_channel_id = 0
+log_channel_id = 0
+
+point_emote_id = 0
 
 # Twtich API
 
-Client_id = '<client id>'
-Oauth2 = '<OAuth token>'
+Client_id = 'Twitch client ID here'
+Oauth2 = 'Auth Token here'
+user_id = 0
+
