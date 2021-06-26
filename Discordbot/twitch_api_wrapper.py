7be8dbfa2d8cd7c0978696f6e57cd3800741f276
YvesCB <y.bruckmayer@gmail.com>
2021-06-26 19:14:32 +0200

    twitch api

--- Discordbot/twitch_api_wrapper.py
+++ Discordbot/twitch_api_wrapper.py
@@ -205,7 +205,7 @@ class Wrapper():
         if broadcaster_id is None:
             params['broadcaster_id'] = self.gg_id
 
-        data = requests.patch(f'https://api.twitch.tv/helix/channels', params=params, headers=self.Headers)
+        data = requests.post(f'https://api.twitch.tv/helix/channels', params=params, headers=self.Headers)
 
         return data.status_code, json.loads(data.text)
 
@@ -227,7 +227,7 @@ class Wrapper():
     def get_clips(self, broadcaster_id=None, game_id=None, clip_id=None, after=None, before=None, ended_at=None, started_at=None, first=20):
         params = {
                 'broadcaster_id': broadcaster_id,
-                'first': frst
+                'first': first
                 }
         if broadcaster_id is None:
             params['broadcaster_id'] = self.gg_id
@@ -244,7 +244,7 @@ class Wrapper():
         if started_at is not None:
             params['started_at'] = started_at
 
-        data = requests.patch(f'https://api.twitch.tv/helix/channels', params=params, headers=self.Headers)
+        data = requests.get(f'https://api.twitch.tv/helix/channels', params=params, headers=self.Headers)
 
         return data.status_code, json.loads(data.text)
 
@@ -260,16 +260,16 @@ class Wrapper():
 
     https://dev.twitch.tv/docs/api/reference#get-top-games
     """
-    def get_top_games(self, after=None, before=None, first=None):
-        params = {}
-        if after is not None:
+    def get_top_games(self, after=None, before=None, first=20):
+        params = {
+            "first": first
+        }
+    if after is not None:
             params['after'] = after
         if before is not None:
             params['before'] = before
-        if first is not None:
-            params['first'] = first
 
-        data = requests.patch(f'https://api.twitch.tv/helix/games/top', params=params, headers=self.Headers)
+        data = requests.get(f'https://api.twitch.tv/helix/games/top', params=params, headers=self.Headers)
 
         return data.status_code, json.loads(data.text)
 
@@ -291,4 +291,66 @@ class Wrapper():
         if game_id is None and game_name is None:
             raise TwitchException('At least one out of the values: game_id, game_name need to be specified!')
 
-        data = requests.patch(f'https://api.twitch.tv/helix/games', params=params, headers=self.Headers)
+        data = requests.get(f'https://api.twitch.tv/helix/games', params=params, headers=self.Headers)
+        
+        return data.status_code, json.loads(data.text)
+
+
+    """
+    Get channel stream schedule for a given channel
+
+    Requires OAuth Token and broadcaster_id
+
+    Optional parameters:
+        "id":           The ID of the stream segment to return
+        "start_time":   A timestamp in RFC3339 format to start returning stream segments from. Default: Current datetime
+        "utc_offset":   Timezone offset in minutes
+        "first":        Max number of stream segments to return. Max: 25, Default: 20
+        "after":        Cursor for pagination
+
+    https://dev.twitch.tv/docs/api/reference#get-channel-stream-schedule
+    """
+    def get_channel_schedule(self, broadcaster_id, segment_id=None, start_time=None, utc_offset=None, first=20, after=None):
+        params = {
+            'broadcaster_id' = broadcaster_id
+            'first' = first
+        }
+        if segment_id is not None:
+            params['id'] = segment_id
+        if start_time is not None:
+            params['start_time'] = start_time
+        if utc_offset is not None:
+            params['utc_offset'] = utc_offset
+        if after is not None:
+            params['after'] = after
+
+        data = requests.get(f'https://api.twitch.tv/helix/schedule', params=params, headers=self.Headers)
+
+        return data.status_code, json.loads(data.text)
+
+
+    """
+    Search categories returns a list of games or cateogires that match the query via name
+
+    Requires a query
+
+    Optional parameters:
+        "first":        Max number of objects to return. Max. 100, Default: 20
+        "after":        Cursfor for forward pagination
+
+    https://dev.twitch.tv/docs/api/reference#search-categories
+    """
+    def search_categories(self, query, first=20, after=None):
+        params = {
+                'query': query
+                'first': first
+                }
+        if after is not None:
+            params['after'] = after
+
+        data = requests.get(f'https://api.twitch.tv/helix/search/categories ', params=params, headers=self.Headers)
+
+        return data.status_code, json.loads(data.text)
+
+
+    
